ae7ecb1633af67be1eaeecba54cff7a7dfb9f911
Puttaraju <puttaraju.r@gmail.com>
2020-04-04 15:22:59 +0000

    virtual distructor tests

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 
 class A {
-  public:
+public:
+
+
 virtual ~A() {
   std::cout<<"~A()"<<std::endl;
 }
 };
 
+class C {
+  public :
+  ~C() {
+        std::cout<<"~C()"<<std::endl;
+  }
+};
+
 class B : public A{
 public:
+C c;
+
 };
+
+
 
 int main() {
   A* a = new B();
